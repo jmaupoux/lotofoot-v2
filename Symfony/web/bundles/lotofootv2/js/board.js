f9c46a4b10bb5d8706e42575e653d5d3da30d1c6
Topich <tom.lann10@gmail.com>
2014-01-20 15:52:48 +0100

    corrections skin comments cagnoute ranking skin board CL in progress

---
+++ Symfony/web/bundles/lotofootv2/js/board.js
@@ -0,0 +1,100 @@
+var tab = 0;
+
+$(document).ready( function () {
+  /*document.getElementById('step_1_1').style.visibility='hidden';*/
+
+	$('#step_1_1').click ( function () {
+		$("#confrontation-step_1_1").fadeIn("fast");
+	});
+	$('#step_1_2').click ( function () {
+		$("#confrontation-step_1_2").fadeIn("fast");
+	});
+	$('#step_1_3').click ( function () {	
+		$("#confrontation-step_1_3").fadeIn("fast");
+	});
+	$('#step_1_4').click ( function () {	
+		$("#confrontation-step_1_4").fadeIn("fast");
+	});
+	$('#step_1_5').click ( function () {	
+		$("#confrontation-step_1_5").fadeIn("fast");
+	});
+	$('#step_1_6').click ( function () {	
+		$("#confrontation-step_1_6").fadeIn("fast");
+	});
+	$('#step_1_7').click ( function () {	
+		$("#confrontation-step_1_7").fadeIn("fast");
+	});
+	$('#step_1_8').click ( function () {	
+		$("#confrontation-step_1_8").fadeIn("fast");
+	});
+	$('#step_2_1').click ( function () {	
+		$("#confrontation-step_2_1").fadeIn("fast");
+	});
+	$('#step_2_2').click ( function () {	
+		$("#confrontation-step_2_2").fadeIn("fast");
+	});
+	$('#step_2_3').click ( function () {	
+		$("#confrontation-step_2_3").fadeIn("fast");
+	});
+	$('#step_2_4').click ( function () {	
+		$("#confrontation-step_2_4").fadeIn("fast");
+	});
+	$('#step_3_1').click ( function () {	
+		$("#confrontation-step_3_1").fadeIn("fast");
+	});
+	$('#step_3_2').click ( function () {	
+		$("#confrontation-step_3_2").fadeIn("fast");
+	});
+	$('#step_4_1').click ( function () {	
+		$("#confrontation-step_4_1").fadeIn("fast");
+	});
+
+$("#confrontation-step_1_1").click( function () {
+		$("#confrontation-step_1_1").fadeOut("slow");
+	});	
+$("#confrontation-step_1_2").click( function () {
+	$("#confrontation-step_1_2").fadeOut("slow");
+});	
+$("#confrontation-step_1_3").click( function () {
+	$("#confrontation-step_1_3").fadeOut("slow");
+});	
+$("#confrontation-step_1_4").click( function () {
+	$("#confrontation-step_1_4").fadeOut("slow");
+});	
+$("#confrontation-step_1_5").click( function () {
+	$("#confrontation-step_1_5").fadeOut("slow");
+});	
+$("#confrontation-step_1_6").click( function () {
+	$("#confrontation-step_1_6").fadeOut("slow");
+});	
+$("#confrontation-step_1_7").click( function () {
+	$("#confrontation-step_1_7").fadeOut("slow");
+});	
+$("#confrontation-step_1_8").click( function () {
+	$("#confrontation-step_1_8").fadeOut("slow");
+});	
+$("#confrontation-step_2_1").click( function () {
+	$("#confrontation-step_2_1").fadeOut("slow");
+});	
+$("#confrontation-step_2_2").click( function () {
+	$("#confrontation-step_2_2").fadeOut("slow");
+});	
+$("#confrontation-step_2_3").click( function () {
+	$("#confrontation-step_2_3").fadeOut("slow");
+});	
+$("#confrontation-step_2_4").click( function () {
+	$("#confrontation-step_2_4").fadeOut("slow");
+});	
+$("#confrontation-step_3_1").click( function () {
+	$("#confrontation-step_3_1").fadeOut("slow");
+});	
+$("#confrontation-step_3_2").click( function () {
+	$("#confrontation-step_3_2").fadeOut("slow");
+});	
+$("#confrontation-step_4_1").click( function () {
+	$("#confrontation-step_4_1").fadeOut("slow");
+});	
+
+
+
+} ) ;
