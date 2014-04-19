2c285f5cbabb4029af81730491b4b84fe670ea53
Tom <Tom@Topich.home>
2014-04-19 17:21:33 +0200

    Ajouts images : menu + r12 + r13 + r14 + r15 Mise a jour du menu avec onglet Coupe du monde

--- Symfony/web/bundles/lotofootv2/js/menu.js
+++ Symfony/web/bundles/lotofootv2/js/menu.js
@@ -3,6 +3,7 @@ var obj = 0;
 $(document).ready( function () {
   $(".menu-navigation ul.submenu-2-championnat").hide();
   $(".menu-navigation ul.submenu-3-championsleague").hide();   
+  $(".menu-navigation ul.submenu-4-coupedumonde").hide();  
   $(".menu-navigation ul.submenu-6-stats").hide();   
 
 $(".menu-navigation li.menu-2-championnat").click( function () {
@@ -12,6 +13,9 @@ $(".menu-navigation li.menu-2-championnat").click( function () {
 			obj=9;}
 		else if (obj==2){
 			$(".menu-navigation ul.submenu-3-championsleague").fadeOut("fast");
+			obj=0;}
+		else if (obj==3){
+			$(".menu-navigation ul.submenu-4-coupedumonde").fadeOut("fast");
 			obj=0;}
 		else {
 			$(".menu-navigation ul.submenu-6-stats").fadeOut("fast");
@@ -35,6 +39,9 @@ $(".menu-navigation li.menu-3-championsleague").click( function () {
 		else if (obj==2){
 			$(".menu-navigation ul.submenu-3-championsleague").fadeOut("fast");
 			obj=9;}
+		else if (obj==3){
+			$(".menu-navigation ul.submenu-4-coupedumonde").fadeOut("fast");
+			obj=0;}
 		else {
 			$(".menu-navigation ul.submenu-6-stats").fadeOut("fast");
 			obj=0;}
@@ -42,6 +49,30 @@ $(".menu-navigation li.menu-3-championsleague").click( function () {
 	if (obj<9){	
 		$(".menu-navigation ul.submenu-3-championsleague").fadeIn("fast");
 		obj=2;}	
+	else{
+		obj=0;
+	}
+});
+
+
+$(".menu-navigation li.menu-4-coupedumonde").click( function () {
+	if (obj>0){
+		if (obj==1){
+			$(".menu-navigation ul.submenu-2-championnat").fadeOut("fast");
+			obj=0;}
+		else if (obj==2){
+			$(".menu-navigation ul.submenu-3-championsleague").fadeOut("fast");
+			obj=0;}
+		else if (obj==3){
+			$(".menu-navigation ul.submenu-4-coupedumonde").fadeOut("fast");
+			obj=9;}
+		else {
+			$(".menu-navigation ul.submenu-6-stats").fadeOut("fast");
+			obj=0;}
+	}
+	if (obj<9){	
+		$(".menu-navigation ul.submenu-4-coupedumonde").fadeIn("fast");
+		obj=3;}	
 	else{
 		obj=0;
 	}
@@ -55,13 +86,16 @@ $(".menu-navigation li.menu-6-stats").click( function () {
 		else if (obj==2){
 			$(".menu-navigation ul.submenu-3-championsleague").fadeOut("fast");
 			obj=0;}
+		else if (obj==3){
+			$(".menu-navigation ul.submenu-4-coupedumonde").fadeOut("fast");
+			obj=0;}
 		else {
 			$(".menu-navigation ul.submenu-6-stats").fadeOut("fast");
 			obj=9;}
 	}
 	if (obj<9){	
 		$(".menu-navigation ul.submenu-6-stats").fadeIn("fast");
-		obj=3;}	
+		obj=4;}	
 	else{
 		obj=0;
 	}
@@ -75,6 +109,9 @@ $(".bg-transparent2").click( function () {
 		$(".menu-navigation ul.submenu-3-championsleague").fadeOut("slow");
 		obj=0;}
 	else if (obj==3){	
+		$(".menu-navigation ul.submenu-4-coupedumonde").fadeOut("slow");
+		obj=0;}
+	else if (obj==4){	
 		$(".menu-navigation ul.submenu-6-stats").fadeOut("slow");
 		obj=0;}
 	});	
