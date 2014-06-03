38807c4786107f74f8ee0681f9247f99fff631c9
Topich <tom.lann10@gmail.com>
2014-06-03 17:11:33 +0200

    Calendar updated

--- Symfony/src/Lotofootv2/Bundle/Controller/User/CalendarController.php
+++ Symfony/src/Lotofootv2/Bundle/Controller/User/CalendarController.php
@@ -21,12 +21,29 @@ class CalendarController extends Controller
     	$days = array();
 
 
-    	array_push($days, new CalendarDay(3, 'cl', new DateTime('2014-04-22 18:00')));
+    	/* array_push($days, new CalendarDay(3, 'cl', new DateTime('2014-04-22 18:00')));
     	array_push($days, new CalendarDay(29, 'l', new DateTime('2014-04-26 13:00')));
     	array_push($days, new CalendarDay(30, 'l', new DateTime('2014-05-03 13:00')));
     	array_push($days, new CalendarDay(31, 'l', new DateTime('2014-05-10 13:00')));
     	array_push($days, new CalendarDay(32, 'l', new DateTime('2014-05-17 13:00')));
-    	array_push($days, new CalendarDay(4, 'cl', new DateTime('2014-05-24 20:00')));
+    	array_push($days, new CalendarDay(4, 'cl', new DateTime('2014-05-24 20:00'))); */
+    	
+    	array_push($days, new CalendarDay(1, 'vote-cup', new DateTime('2014-06-12 22:00')));
+    	array_push($days, new CalendarDay(2, 'vote-cup', new DateTime('2014-06-13 18:00')));
+    	array_push($days, new CalendarDay(3, 'vote-cup', new DateTime('2014-06-13 21:00')));
+    	array_push($days, new CalendarDay(4, 'vote-cup', new DateTime('2014-06-13 23:59')));
+    	array_push($days, new CalendarDay(5, 'vote-cup', new DateTime('2014-06-14 03:00')));
+    	array_push($days, new CalendarDay(6, 'vote-cup', new DateTime('2014-06-14 18:00')));
+    	array_push($days, new CalendarDay(7, 'vote-cup', new DateTime('2014-06-14 21:00')));
+    	array_push($days, new CalendarDay(8, 'vote-cup', new DateTime('2014-06-14 23:59')));
+    	array_push($days, new CalendarDay(9, 'vote-cup', new DateTime('2014-06-15 18:00')));
+    	array_push($days, new CalendarDay(10, 'vote-cup', new DateTime('2014-06-15 21:00')));
+    	array_push($days, new CalendarDay(11, 'vote-cup', new DateTime('2014-06-15 23:59')));
+    	array_push($days, new CalendarDay(12, 'vote-cup', new DateTime('2014-06-16 18:00')));
+    	array_push($days, new CalendarDay(13, 'vote-cup', new DateTime('2014-06-16 21:00')));
+    	array_push($days, new CalendarDay(14, 'vote-cup', new DateTime('2014-06-16 23:59')));
+    	array_push($days, new CalendarDay(15, 'vote-cup', new DateTime('2014-06-17 18:00')));
+    	array_push($days, new CalendarDay(16, 'vote-cup', new DateTime('2014-06-17 23:59')));
     	
     	for($i=0;$i<count($days);$i++){
     		if($days[$i]->getBefore() < new DateTime()){
