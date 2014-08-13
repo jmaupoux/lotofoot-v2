81ee280e93e9a3b83e787403951032dee1928851
Topich <tom.lann10@gmail.com>
2014-08-13 17:42:27 +0200

    Rewards + fix bugs + tabs

--- Symfony/src/Lotofootv2/Bundle/Controller/User/Stats/RewardsController.php
+++ Symfony/src/Lotofootv2/Bundle/Controller/User/Stats/RewardsController.php
@@ -9,12 +9,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class RewardsController extends Controller
 {
 	/**
-     * @Route("/stats/rewards", name="_stats_rewards")
-     */
-    public function indexAction()
-    {
-    	$rewards = $this->get('reward_service')->getRewardedAccounts();
- 
-       return $this->render('Lotofootv2Bundle:User/Stats:rewards.html.twig', array('rewards' => $rewards));
-    }
+	 * @Route("/stats/rewards", name="_stats_rewards")
+	 */
+	public function indexAction()
+	{
+		$rewards = $this->get('reward_service')->getRewardedAccounts();
+		$accounts = $this->get('account_service')->all();
+
+
+		return $this->render('Lotofootv2Bundle:User/Stats:rewards.html.twig', array('rewards' => $rewards, 'users'=>$accounts));
+	}
+
 }
