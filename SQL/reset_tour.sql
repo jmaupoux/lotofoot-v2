0e5a878df9185cff71d47078b3ae5ef1658c1e81
jmaupoux <jmaupoux@gmail.com>
2014-08-20 19:30:07 +0200

    delete deprecated dumps

--- SQL/reset_tour.sql
+++
@@ -1,6 +0,0 @@
-delete from lfv2_tournament;
-delete from lfv2_tournament_step;
-delete from lfv2_tournament_player;
-delete from lfv2_tournament_vote;
-delete from lfv2_tournament_match;
-delete from lfv2_tournament_history;
