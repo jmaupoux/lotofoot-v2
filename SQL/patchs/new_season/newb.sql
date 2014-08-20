a6256d98a45c069715b9c8248de54c2012da0c82
jmaupoux <jmaupoux@gmail.com>
2014-08-20 19:28:49 +0200

    Multi executable script

--- SQL/patchs/new_season/newb.sql
+++ SQL/patchs/new_season/newb.sql
@@ -1,3 +1,3 @@
 INSERT INTO lfv2_reward (account_id, reward_id, type)
-	select a.id as account_id, 17 as reward_id, 's' as type from lfv2_account a where (select count(1) from lfv2_league_history h  where h.account_id = a.id and h.points != 0 and h.season = 1 group by h.account_id) < 3 or not 
-	exists( select * from lfv2_league_history h  where h.account_id = a.id and h.season = 1)
+	select a.id as account_id, 17 as reward_id, 's' as type from lfv2_account a where not exists(select 1 from lfv2_reward where reward_id = 17 and account_id = a.id) and ((select count(1) from lfv2_league_history h  where h.account_id = a.id and h.points != 0 and h.season = 1 group by h.account_id) < 3 or not 
+	exists( select * from lfv2_league_history h  where h.account_id = a.id and h.season = 1))
