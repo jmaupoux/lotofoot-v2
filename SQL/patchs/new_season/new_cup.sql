1cf1ae6bdb189a27e07dcd59bb2d681e3a2b56ac
Julien Maupoux <jmaupoux@gmail.com>
2018-04-22 16:47:00 +0200

    [Fix] Ajout reset rewards

--- SQL/patchs/new_season/new_cup.sql
+++ SQL/patchs/new_season/new_cup.sql
@@ -2,3 +2,4 @@ update lfv2_account set cup_cagnoute = false, cup_points = 0, cup_rank = 99, sta
 delete from lfv2_cup_vote;
 delete from lfv2_cup_match;
 update lfv2_cup set name = 'EURO 2016';
+delete from lfv2_reward;
